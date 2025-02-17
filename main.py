0bcb28e75ceef2ed6eb4d8dc2a603a1e8abf3a19
YO_DI3G0 <diegocda1212@gmail.com>
2025-02-17 17:59:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ print("""
 ╚═╝  ╚═══╝╚══════╝╚═════╝  ╚═════╝ ╚══════╝╚═╝  ╚═╝       ╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝
 """)
 
-close_program = input("Do you want to close this program? y/n: ")
-if close_program.lower() == 'y':
-    print("Closing program...")
-else:
-    print("Program will keep running.")
+while true:
+    close_program = input("Do you want to close this program? y/n: ").lower()
+    if close_program == 'y':
+        print("Closing program...")
+        break
+    elif close_program == 'n':
+        print("Program will keep running.")
+    else:
+        print("Invalid input. Please enter 'y' to close or 'n' to keep running the program.")
+    
